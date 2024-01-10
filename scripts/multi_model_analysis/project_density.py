75aae930dcd77d3df27d4bb6b3cd9463c61517f6
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-01-10 16:14:12 +0000

    p

--- scripts/multi_model_analysis/project_density.py
+++ scripts/multi_model_analysis/project_density.py
@@ -29,8 +29,53 @@ data = {
                     },
 
                 }
+        },
+    "JMJD2DA":
+        {
+            "ModelDir": Path('../../Downloads/pandda_BAZ2BA_all_models/processed_datasets'),
+            "Datasets":
+                {
+                    "JMJD2DA-x427": {
+                        "ModelPath": "autobuild/2_13_ligand_0.pdb",
+                        "CifPath": 'ligand_files/ligand.cif',
+                    },
+                    "JMJD2DA-x390": {
+                        "ModelPath": "autobuild/1_24_ligand_0.pdb",
+                        "CifPath": 'ligand_files/ligand.cif'
+                    },
+                    "JMJD2DA-x533": {
+                        "ModelPath": "autobuild/1_2_ligand_0.pdb",
+                        "CifPath": 'ligand_files/ligand.cif'
+                    },
+                    "JMJD2DA-x334": {
+                        "ModelPath": "autobuild/3_2_ligand_0.pdb",
+                        "CifPath": 'ligand_files/ligand.cif'
+                    },
+
+                }
+        },
+    "NSP16":
+        {
+            "ModelDir": Path('/dls/labxchem/data/2020/lb26998-2/processing/analysis/pandda_2_new_hits/processed_datasets'),
+            "Datasets":
+                {
+                    "NSP16-x0165": {
+                        "ModelPath": "autobuild/1_4_ligand_0.pdb",
+                        "CifPath": 'ligand_files/ligand.cif',
+                    },
+                    "NSP16-x0145": {
+                        "ModelPath": "autobuild/2_4_ligand_0.pdb",
+                        "CifPath": 'ligand_files/ligand.cif'
+                    },
+                    "NSP16-x0501": {
+                        "ModelPath": "autobuild/3_7_ligand_0.pdb",
+                        "CifPath": 'ligand_files/ligand.cif'
+                    },
+
+                }
         }
 }
+
 
 def main(
         # model_path,
