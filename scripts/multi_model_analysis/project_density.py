90e63be3940d1206c9526a13ef0133071d77b2e3
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-01-10 16:38:49 +0000

    p

--- scripts/multi_model_analysis/project_density.py
+++ scripts/multi_model_analysis/project_density.py
@@ -6,78 +6,78 @@ from dlib.dplotting.plot_projection import plot_projection
 
 
 data = {
-    "BAZ2BA":
-        {
-            "ModelDir": Path('../../Downloads/pandda_BAZ2BA_all_hits_all_models/processed_datasets'),
-            "Datasets":
-                {
-                    "BAZ2BA-x446": {
-                        "ModelPath": "autobuild/1_4_ligand_0.pdb",
-                        "CifPath": 'ligand_files/ligand.cif',
-                    },
-                    "BAZ2BA-x557": {
-                        "ModelPath": "autobuild/2_4_ligand_0.pdb",
-                        "CifPath": 'ligand_files/ligand.cif'
-                    },
-                    "BAZ2BA-x529": {
-                        "ModelPath": "autobuild/3_7_ligand_0.pdb",
-                        "CifPath": 'ligand_files/ligand.cif'
-                    },
-                    "BAZ2BA-x583": {
-                        "ModelPath": "autobuild/2_4_ligand_0.pdb",
-                        "CifPath": 'ligand_files/ligand.cif'
-                    },
-
-                }
-        },
-    "JMJD2DA":
-        {
-            "ModelDir": Path('../../Downloads/pandda_JMJD2DA_all_models/processed_datasets'),
-            "Datasets":
-                {
-                    "JMJD2DA-x427": {
-                        "ModelPath": "autobuild/2_13_ligand_0.pdb",
-                        "CifPath": 'ligand_files/ligand.cif',
-                    },
-                    "JMJD2DA-x390": {
-                        "ModelPath": "autobuild/1_24_ligand_0.pdb",
-                        "CifPath": 'ligand_files/ligand.cif'
-                    },
-                    # "JMJD2DA-x390": {
-                    #     "ModelPath": "autobuild/1_3_ligand_0.pdb",
-                    #     "CifPath": 'ligand_files/ligand.cif'
-                    # },
-                    "JMJD2DA-x533": {
-                        "ModelPath": "autobuild/1_2_ligand_0.pdb",
-                        "CifPath": 'ligand_files/ligand.cif'
-                    },
-                    "JMJD2DA-x334": {
-                        "ModelPath": "autobuild/3_2_ligand_0.pdb",
-                        "CifPath": 'ligand_files/ligand.cif'
-                    },
-
-                }
-        },
-    # "NSP16":
+    # "BAZ2BA":
     #     {
-    #         "ModelDir": Path('/dls/labxchem/data/2020/lb26998-2/processing/analysis/pandda_2_new_hits/processed_datasets'),
+    #         "ModelDir": Path('../../Downloads/pandda_BAZ2BA_all_hits_all_models/processed_datasets'),
     #         "Datasets":
     #             {
-    #                 "NSP16-x0165": {
+    #                 "BAZ2BA-x446": {
     #                     "ModelPath": "autobuild/1_4_ligand_0.pdb",
     #                     "CifPath": 'ligand_files/ligand.cif',
     #                 },
-    #                 "NSP16-x0145": {
+    #                 "BAZ2BA-x557": {
     #                     "ModelPath": "autobuild/2_4_ligand_0.pdb",
     #                     "CifPath": 'ligand_files/ligand.cif'
     #                 },
-    #                 "NSP16-x0501": {
+    #                 "BAZ2BA-x529": {
     #                     "ModelPath": "autobuild/3_7_ligand_0.pdb",
+    #                     "CifPath": 'ligand_files/ligand.cif'
+    #                 },
+    #                 "BAZ2BA-x583": {
+    #                     "ModelPath": "autobuild/2_4_ligand_0.pdb",
     #                     "CifPath": 'ligand_files/ligand.cif'
     #                 },
     #
     #             }
-    #     }
+    #     },
+    # "JMJD2DA":
+    #     {
+    #         "ModelDir": Path('../../Downloads/pandda_JMJD2DA_all_models/processed_datasets'),
+    #         "Datasets":
+    #             {
+    #                 "JMJD2DA-x427": {
+    #                     "ModelPath": "autobuild/2_13_ligand_0.pdb",
+    #                     "CifPath": 'ligand_files/ligand.cif',
+    #                 },
+    #                 "JMJD2DA-x390": {
+    #                     "ModelPath": "autobuild/1_24_ligand_0.pdb",
+    #                     "CifPath": 'ligand_files/ligand.cif'
+    #                 },
+    #                 # "JMJD2DA-x390": {
+    #                 #     "ModelPath": "autobuild/1_3_ligand_0.pdb",
+    #                 #     "CifPath": 'ligand_files/ligand.cif'
+    #                 # },
+    #                 "JMJD2DA-x533": {
+    #                     "ModelPath": "autobuild/1_2_ligand_0.pdb",
+    #                     "CifPath": 'ligand_files/ligand.cif'
+    #                 },
+    #                 "JMJD2DA-x334": {
+    #                     "ModelPath": "autobuild/3_2_ligand_0.pdb",
+    #                     "CifPath": 'ligand_files/ligand.cif'
+    #                 },
+    #
+    #             }
+    #     },
+    "NSP16":
+        {
+            "ModelDir": Path('/dls/labxchem/data/2020/lb26998-2/processing/analysis/pandda_2_new_hits/processed_datasets'),
+            "Datasets":
+                {
+                    "NSP16-x0165": {
+                        "ModelPath": "autobuild/4_16_Z1250132544_4.pdb",
+                        "CifPath": 'ligand_files/ligand.cif',
+                    },
+                    # "NSP16-x0145": {
+                    #     "ModelPath": "autobuild/2_4_ligand_0.pdb",
+                    #     "CifPath": 'ligand_files/ligand.cif'
+                    # },
+                    # "NSP16-x0501": {
+                    #     "ModelPath": "autobuild/3_7_ligand_0.pdb",
+                    #     "CifPath": 'ligand_files/ligand.cif'
+                    # },
+
+                }
+        }
 }
 
 
