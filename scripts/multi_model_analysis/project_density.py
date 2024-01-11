bcb34bca033490d3f3b9b0cfccd99e3780b7b041
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2024-01-11 12:36:51 +0000

    p

--- scripts/multi_model_analysis/project_density.py
+++ scripts/multi_model_analysis/project_density.py
@@ -84,7 +84,7 @@ data = {
             "Datasets":
                 {
                     "Mpro-x0089": {
-                        "ModelPath": "autobuild/7_4_Z1650168321_0.pdb",
+                        "ModelPath": "autobuild/7_4_Z1650168321_0-coot-0.pdb",
                         "CifPath": 'ligand_files/Z1650168321.cif',
                     },
                 }
